372a498e05ae2a01ca32a60e57bfd29f837a0b63
wichapol jaimaung <wichapol.44424@gmail.com>
2022-05-16 10:25:48 +0700

    Create Component

---
+++ frontend/template-styling/src/components/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import PostCard from '../PostCard/PostCard';
+
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <PostCard />
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/template-styling/src/components/PostCard/PostCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './PostCard.css';
+
+const PostCard = () => {
+    return (
+        <>
+        <h1> Hello World</h1>
+        </>
+    );
+};
+export default PostCard;
